63070b57e256fc52832981d8ec5329c6980375a9
LevelNico <quellmoell@gmail.com>
2015-04-28 14:46:18 +0200

    Finished Collision
    Collision now works optimally. Also started developing a map reading
    class.

--- Game of OOSE/src/med4/game_of_oose/gamestate/Level1State.java
+++ Game of OOSE/src/med4/game_of_oose/gamestate/Level1State.java
@@ -3,10 +3,14 @@ package med4.game_of_oose.gamestate;
 import java.awt.Graphics;
 
 import med4.game_of_oose.entities.Player;
+import med4.game_of_oose.main.ApplicationPanel;
+import med4.game_of_oose.mapping.Map;
+import med4.game_of_oose.objects.Block;
 
 public class Level1State extends ApplicationState {
 	
 	private Player player;
+	private Map map;
 	
 	public Level1State(ApplicationStateManager asm) {
 		super(asm);
@@ -15,18 +19,22 @@ public class Level1State extends ApplicationState {
 	
 	public void init() {
 		player = new Player(30, 30);
+		map = new Map("",4,4);
+		
+		xOffset =- 200;
+		yOffset =- 400;
 	}
 
 	
 	public void tick() {
-		player.tick();
+		player.tick(map.getBlocks());
 	}
 
 	
 	public void draw(Graphics g){
-		g.clearRect(0, 0, 80,80);
+		g.clearRect(0, 0, ApplicationPanel.WIDTH, ApplicationPanel.HEIGHT);
 		player.draw(g);
-		
+		map.draw(g);
 	}
 
 	
--- Game of OOSE/src/med4/game_of_oose/objects/Block.java
+++ Game of OOSE/src/med4/game_of_oose/objects/Block.java
@@ -3,15 +3,21 @@ package med4.game_of_oose.objects;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
+import med4.game_of_oose.gamestate.ApplicationState;
+
 public class Block extends Rectangle {
 	public static final long serialVersionUID= 1L;
 	
-	public static final int blockSize = 32;
+	public static final int blockSize = 64;
 	public Block(int x, int y){
 		setBounds(x,y,blockSize,blockSize);
 	}
 	
+	public void tick() {
+		
+	}
+	
 	public void draw(Graphics g){
-		g.fillRect(x, y, width, height);
+		g.fillRect(x - (int)ApplicationState.xOffset, y - (int)ApplicationState.yOffset, width, height);
 	}
 }
